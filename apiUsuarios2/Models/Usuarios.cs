85116a2ae5cf7c2a69b0a694ee5f90ec828d0f2a
cberto <bertolanicami@gmail.com>
2022-02-19 13:28:55 -0300

    Login

---
+++ apiUsuarios2/Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiUsuarios2.Models
+{
+    [Table("usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
